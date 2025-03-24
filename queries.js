6ef6c90eafb13c20fe7b6c93b9c5959e7697fdd9
Mari <43574832+marisoto@users.noreply.github.com>
2025-03-24 02:16:30 -0400

    Add files via upload

---
+++ queries.js
@@ -0,0 +1,9 @@
+requestAnimationFrame('dotenv').config();
+
+const Pool = require('pg').Pool
+const pool = new Pool ({
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PORT,
+})
